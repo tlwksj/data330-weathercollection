b17a684aa45b487a6b836c21c216f8500a03d0d7
Trishelle Leal <166420279+tlwksj@users.noreply.github.com>
2025-11-19 19:08:19 -0500

    Update get_emmitsburg_weather.R
    Protect from when API fails

--- get_emmitsburg_weather.R
+++ get_emmitsburg_weather.R
@@ -4,31 +4,36 @@ library(httr)
 library(jsonlite)
 library(readr)
 
-#res <- GET("https://api.weather.gov/stations/KHGR/observations/latest")
-data <- fromJSON("https://api.weather.gov/stations/KHGR/observations/latest")
-
-temp_c <- data$properties$temperature$value
-temp_f <- temp_c * 9/5 +32
-
-wind_speed_km <- data$properties$windSpeed$value
-wind_speed_mi <- round(ifelse(is.null(wind_speed_km), 0, wind_speed_km) / 1.6,3)
-
-df <- data.frame(
-  time = format(Sys.time(), tz = "America/New_York", usetz = TRUE),
-  station = "KHGR",
-  temp_c = temp_c,
-  temp_f = temp_f,
-  wind_speed_km = wind_speed_km,
-  wind_speed_mi = wind_speed_mi
-)
-
-dir.create("data", showWarnings = FALSE)
-
-file <- "data/emmitsburg_weather.csv"
-
-if(file.exists(file)){
-  write_csv(df, file, append = TRUE)
-} else{
-  write_csv(df,file)
+res <- GET("https://api.weather.gov/stations/KHGR/observations/latest")
+if(status_code(res) == 200){
+  data <- fromJSON("https://api.weather.gov/stations/KHGR/observations/latest")
+  
+  temp_c <- data$properties$temperature$value
+  temp_f <- temp_c * 9/5 +32
+  
+  wind_speed_km <- data$properties$windSpeed$value
+  wind_speed_mi <- round(ifelse(is.null(wind_speed_km), 0, wind_speed_km) / 1.6,3)
+  
+  df <- data.frame(
+    time = format(Sys.time(), tz = "America/New_York", usetz = TRUE),
+    station = "KHGR",
+    temp_c = temp_c,
+    temp_f = temp_f,
+    wind_speed_km = wind_speed_km,
+    wind_speed_mi = wind_speed_mi
+  )
+  
+  dir.create("data", showWarnings = FALSE)
+  
+  file <- "data/emmitsburg_weather.csv"
+  
+  if(file.exists(file)){
+    write_csv(df, file, append = TRUE)
+  } else{
+    write_csv(df,file)
+  }
+}
+else {
+message("API call failed")
 }
 
